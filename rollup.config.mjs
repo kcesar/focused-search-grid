d5821adef947ddb36f8955a20d5835d2358e8d4a
Anil Kulkarni <anil@terminal.space>
2023-02-19 14:01:09 -0800

    Switch from webpack to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import resolve from '@rollup/plugin-node-resolve';
+import path from 'path';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import scss from 'rollup-plugin-scss';
+import { generateSW } from 'rollup-plugin-workbox';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+//https://github.com/thgh/rollup-plugin-scss/issues/12#issuecomment-791476941
+const importer = (url, _prev, done) => {
+  if (url[0] !== '~') {
+    return null;
+  }
+  const info = { file: path.resolve(`node_modules/${url.substr(1)}`) };
+  if (done) {
+    done(info);
+  }
+  return info;
+};
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'dist',
+    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
+  },
+  plugins: [
+    scss({
+      importer,
+      output: 'dist/style.css',
+      outputStyle: production ? 'compressed' : 'expanded',
+    }),
+    resolve(),
+    commonjs(),
+    generateSW({
+      swDest: 'dist/service-worker.js',
+      globDirectory: 'dist/',
+    }),
+    production && terser(), // minify, but only in production
+  ],
+};
